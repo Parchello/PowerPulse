c04d8fa74b8571588c6e7ed088a4097f77ff9091
a.panasenko <andrii.panasenko295@gmail.com>
2024-03-01 11:41:10 +0200

    add theme provider

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,17 @@ import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/PowerPulse">
-        <App />
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter basename="/PowerPulse">
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
