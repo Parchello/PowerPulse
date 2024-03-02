8de2845dfdded29a41c73e1bdf98bacf71b8e1da
AnastasiiaSyplyvets <102216774+AnastasiiaSyplyvets@users.noreply.github.com>
2024-03-02 14:57:36 +0100

    2

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,32 +1,39 @@
 import TitlePage from '../ProfilePage/TitlePage';
 import UserForm from '../ProfilePage/UserForm';
+// import Usercard from '../ProfilePage/UserCard';
 import styled from 'styled-components';
 
 const Section = styled.div`
-  margin-top: 72px;
-  margin-left: 96px;
-  margin-bottom: 44px;
+  padding-top: 72px;
+  padding-left: 96px;
+  padding-right: 96px;
+  padding-bottom: 44px;
 
   //mobile
   @media screen and (min-width: 320px) and (max-width: 375px) {
-    margin-top: 40px;
-    margin-left: 20px;
-    margin-right: 20px;
+    padding-top: 40px;
+    padding-left: 20px;
+    padding-right: 20px;
   }
 
   //tablet
 
   @media screen and (min-width: 764px) and (max-width: 1439px) {
-    margin-left: 32px;
+    padding-left: 32px;
+    padding-right: 40px;
+    padding-bottom: 54px;
   }
 `;
 
 const ProfilePage = () => {
   return (
-    <Section>
-      <TitlePage titleName="Profile Settings" />
-      <UserForm />
-    </Section>
+    <>
+      <Section>
+        <TitlePage titleName="Profile Settings" />
+        <UserForm />
+        {/* <Usercard /> */}
+      </Section>
+    </>
   );
 };
 
