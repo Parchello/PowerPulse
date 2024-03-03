4e95249f1046de42ed02c87b32a8bafc3e63091c
AndriyMerzlikin <lastheroofme@gmail.com>
2024-03-03 12:09:06 +0200

    111

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import SharedLayout from 'components/SharedLayout/SharedLayout';
 
 import ErrorPage from 'pages/ErrorPage/ErrorPage';
 
+import PrivateRoute from './route/PrivateRoute';
 import WelcomePage from './pages/WelcomePage/WelcomePage';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
 import LoginPage from './pages/LoginPage/LoginPage';
+import UserPage from './pages/UserPage/UserPage';
+import DiaryPage from './pages/DiaryPage/DiaryPage';
+import ProductsPage from './pages/ProductsPage/ProductsPage';
+import ExercisesPage from './pages/ExercisesPage/ExercisesPage';
 // import ProfilePage from './pages/ProfilePage/ProfilePage';
 
 function App() {
@@ -16,6 +21,12 @@ function App() {
           <Route index element={<WelcomePage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />}></Route>
+          <Route element={<PrivateRoute />}>
+            <Route path="/user" element={<UserPage />} />
+            <Route path="/diary" element={<DiaryPage />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/exercises" element={<ExercisesPage />} />
+          </Route>
           <Route path="*" element={<ErrorPage />} />
           {/* <Route path="/profile" element={<ProfilePage />} /> */}
         </Route>
--- src/route/PrivateRoute.jsx
+++ src/route/PrivateRoute.jsx
@@ -1,9 +1,8 @@
-import { useState } from 'react';
-import { Navigate } from 'react-router-dom';
+// import { useState } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
 
-import { Outlet } from 'react-router-dom/dist';
-
-export const PrivateRoute = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(true);
+export default function PrivateRoute() {
+  // const [isAuthenticated, setIsAuthenticated] = useState(true);
+  const isAuthenticated = true;
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
-};
+}
