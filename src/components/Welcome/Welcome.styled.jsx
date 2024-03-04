90cb612f2fe5699a92a90c9a078d52d03a2150dd
a.panasenko <andrii.panasenko295@gmail.com>
2024-03-04 22:27:54 +0200

    deteled old file and commented lines

--- src/components/Welcome/Welcome.styled.jsx
+++ src/components/Welcome/Welcome.styled.jsx
@@ -1,26 +1,14 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
-  /* width: 100%;
-  padding-left: 20px;
-  padding-right: 20px; */
   padding-bottom: 40px;
-  /* margin: 0 auto; */
-  outline: 2px solid red;
-
-  /* max-width: 375px; */
+  /* outline: 2px solid red; */
 
   @media screen and (min-width: 768px) {
-    /* max-width: 768px;
-    padding-left: 32px;
-    padding-right: 32px; */
-
     padding-bottom: 48px;
   }
 
   @media screen and (min-width: 1440px) {
-    /* max-width: 1440px;
-    padding-left: 96px; */
     padding-right: 0;
     padding-bottom: 171px;
 
