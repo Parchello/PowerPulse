e884a0cb35e3f696e825d0fe6bcae98ab02eb112
AndriyMerzlikin <lastheroofme@gmail.com>
2024-03-04 16:43:56 +0200

    products fixed svg

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,56 +1,13 @@
-// import { HeroLayout } from '../HeroLeyout/HeroLayout';
-import sprite from '../../assets/sprite.svg';
-import {
-  Container,
-  DietBlock,
-  DietBlockContainer,
-  Filters,
-  FiltersContainer,
-  MainText,
-  SearchInput,
-  SelectorA,
-  SelectorBox,
-  SelectorC,
-  SvgIcon,
-} from './Products.styled';
+import { DietCardsList } from './DietCardsList/DietCardsList';
+import { Container, MainText } from './Products.styled';
+import { ProductsFilter } from './ProductsFilter/ProductsFilter';
 
 export const Products = () => {
   return (
     <Container>
-      <MainText>Products</MainText>
-      <FiltersContainer>
-        <Filters>
-          <div>
-            <SearchInput type="text" placeholder="Search" />
-            <SvgIcon width="18px" height="18px">
-              <use xlinkHref={sprite + '#icon-search'} />
-            </SvgIcon>
-          </div>
-          <SelectorBox>
-            <SelectorC name="Categories" id="cat" placeholder="Categories">
-              <option value disabled selected>
-                Categories
-              </option>
-            </SelectorC>
-            <SelectorA name="all" id="all">
-              <option value disabled selected>
-                All
-              </option>
-            </SelectorA>
-          </SelectorBox>
-        </Filters>
-      </FiltersContainer>
-
-      <DietBlockContainer>
-        <DietBlock></DietBlock>
-        <DietBlock></DietBlock>
-        <DietBlock></DietBlock>
-        <DietBlock></DietBlock>
-        <DietBlock></DietBlock>
-        <DietBlock></DietBlock>
-        <DietBlock></DietBlock>
-        <DietBlock></DietBlock>
-      </DietBlockContainer>
+      <MainText>Products 111</MainText>
+      <ProductsFilter />
+      <DietCardsList />
     </Container>
   );
 };
--- src/components/Products/Products.styled.jsx
+++ src/components/Products/Products.styled.jsx
@@ -35,82 +35,14 @@ export const MainText = styled.div`
   line-height: 117%;
   margin-top: 40px;
   margin-bottom: 40px;
-`;
 
-export const FiltersContainer = styled.div`
-  margin-bottom: 40px;
-`;
-
-export const Filters = styled.div`
-  width: 335px;
-  height: 108px;
-`;
-
-export const SvgIcon = styled.svg`
-  position: relative;
-  z-index: 1;
-  float: right;
-  bottom: 32px;
-  right: 14px;
-`;
-
-export const SearchInput = styled.input`
-  background-color: black;
-  border: 1px solid rgba(239, 237, 232, 0.3);
-  border-radius: 12px;
-  padding: 14px;
-  width: 335px;
-  height: 46px;
-
-  &::placeholder {
-    color: #efede8;
+  @media screen and (min-width: 768px) {
+    margin-top: 72px;
+    margin-bottom: 32px;
+    font-size: 32px;
+    line-height: 137%;
   }
-`;
 
-export const SelectorBox = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 335px;
-  height: 46px;
-`;
-
-export const SelectorC = styled.select`
-  background-color: black;
-  color: #efede8;
-  border: 1px solid rgba(239, 237, 232, 0.3);
-  border-radius: 12px;
-  padding: 14px;
-  width: 146px;
-  height: 46px;
-`;
-
-export const SelectorA = styled.select`
-  background-color: black;
-  color: #efede8;
-  border: 1px solid rgba(239, 237, 232, 0.3);
-  border-radius: 12px;
-  padding: 14px;
-  width: 173px;
-  height: 46px;
-`;
-
-export const DietBlockContainer = styled.li`
-  list-style: none;
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-`;
-
-export const DietBlock = styled.li`
-  /* display: flex;
-  flex-direction: column;
-  gap: 20px; */
-  width: 335px;
-  height: 141px;
-  border: 1px solid rgba(239, 237, 232, 0.2);
-  border-radius: 12px;
-  padding: 140px 105px;
-  width: 335px;
-  height: 141px;
-  background: rgba(239, 237, 232, 0.05);
+  @media screen and (min-width: 1440px) {
+  }
 `;
