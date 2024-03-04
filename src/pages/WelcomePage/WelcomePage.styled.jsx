50ca48b5f086b0d320c954c50e5214b45fcbc398
a.panasenko <andrii.panasenko295@gmail.com>
2024-03-04 20:45:48 +0200

    Update WelcomePage.styled.jsx

--- src/pages/WelcomePage/WelcomePage.styled.jsx
+++ src/pages/WelcomePage/WelcomePage.styled.jsx
@@ -2,27 +2,27 @@ import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 export const Container = styled.div`
-  width: 100%;
+  /* width: 100%;
   padding-left: 20px;
-  padding-right: 20px;
+  padding-right: 20px; */
   padding-bottom: 40px;
-  margin: 0 auto;
-  /* outline: 2px solid red; */
+  /* margin: 0 auto; */
+  outline: 2px solid red;
 
-  max-width: 375px;
+  /* max-width: 375px; */
 
   @media screen and (min-width: 768px) {
-    max-width: 768px;
+    /* max-width: 768px;
     padding-left: 32px;
-    padding-right: 32px;
+    padding-right: 32px; */
 
     padding-bottom: 48px;
   }
 
   @media screen and (min-width: 1440px) {
-    max-width: 1440px;
-    padding-left: 96px;
-    padding-right: 32px;
+    /* max-width: 1440px;
+    padding-left: 96px; */
+    padding-right: 0;
     padding-bottom: 171px;
 
     display: flex;
@@ -302,6 +302,7 @@ export const SvgRunWrapper = styled.div`
 `;
 
 export const SvgRun = styled.svg`
+  fill: ${(p) => p.theme.colors.white};
   width: 12px;
   height: 12px;
 
