2cd7773701f34d0af81dc08e0319fe6faf22a73d
Parchello <parchellos@gmail.com>
2024-03-05 16:56:59 +0200

    more styles

--- src/components/Diary/DayExercises/DayExercises.styled.jsx
+++ src/components/Diary/DayExercises/DayExercises.styled.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const DairyExercisesField = styled.div`
@@ -14,11 +15,35 @@ export const ExerciseBlock = styled.div`
   display: flex;
 `;
 
-export const AddExerciseBtn = styled.a`
+export const ExersLink = styled(Link)`
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 129%;
+  color: ${(props) => props.theme.colors.orange};
   margin: 0;
   padding: 0;
   display: flex;
   justify-content: flex-end;
   margin-left: auto;
-  color: orange;
+`;
+
+export const ExersTitle = styled.p`
+  font-size: 14px;
+  line-height: 129%;
+  color: rgba(239, 237, 232, 0.5);
+`;
+
+export const ExersBtn = styled.div`
+  margin-left: 16px;
+  display: flex;
+  gap: 8px;
+  padding: 0;
+  width: 49px;
+  height: 18px;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 129%;
+  color: ${(props) => props.theme.colors.orange};
+  background-color: rgba(255, 255, 255, 0);
+  border: none;
 `;
