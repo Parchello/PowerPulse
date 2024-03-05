579786b6ed01839f1efd278587e4553dae34b1ab
Shkliarova <shklyarovaalisa@gmail.com>
2024-03-05 20:57:53 +0200

    add padding-bottom

--- src/components/SignUp/RegisterPage.styled.jsx
+++ src/components/SignUp/RegisterPage.styled.jsx
@@ -2,14 +2,15 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     padding-top: 127px;
+    padding-bottom: 40px;
 
     @media screen and (min-width: 768px) {
     padding-top: 189px;
-    /* padding-bottom: 349px; //check right values */
+    padding-bottom: 48px; 
   }
 
     @media screen and (min-width: 1440px) {
     display: flex;
     padding-top: 200px; 
-    /* padding-bottom: 114px; //check right values */
+    padding-bottom: 171px; 
 }`
