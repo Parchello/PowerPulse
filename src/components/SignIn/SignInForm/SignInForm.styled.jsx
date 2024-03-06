1f57358f847a00ce8a17804f2c6aeb84d71b1df7
AnastasiiaSyplyvets <102216774+AnastasiiaSyplyvets@users.noreply.github.com>
2024-03-06 21:14:33 +0100

    7

--- src/components/SignIn/SignInForm/SignInForm.styled.jsx
+++ src/components/SignIn/SignInForm/SignInForm.styled.jsx
@@ -1,55 +1,57 @@
 import {
-    Form as FormikForm,
-    Field as FormikField,
-    ErrorMessage as FormikError,
-  } from 'formik';
-  import styled from 'styled-components';
-  
-  export const Form = styled(FormikForm)`
-    display: flex;
-    flex-direction: column;
-    gap: 14px;
+  Form as FormikForm,
+  Field as FormikField,
+  ErrorMessage as FormikError,
+} from 'formik';
+import styled from 'styled-components';
 
-    @media screen and (min-width: 768px) {
-     gap: 20px;
-    }
-  `;
-  
-  export const Field = styled(FormikField)`
+export const Form = styled(FormikForm)`
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+
+  @media screen and (min-width: 768px) {
+    gap: 20px;
+  }
+`;
+
+export const Field = styled(FormikField)`
   background-color: rgba(0, 0, 0, 0);
-border: 1px solid rgba(239, 237, 232, 0.3);
-border-radius: 12px;
-padding: 14px 200px 14px 14px;
-width: 335px;
-height: 46px;
-&::placeholder { font-weight: 400;
-  font-size: 14px;
-  line-height: 129%;
-  color: ${(p) => p.theme.colors.white};
-  opacity: 30%;}
-  &:hover, :focus {
-    border-color: ${(p)=>p.theme.colors.orange} ;
-
-  };
+  border: 1px solid rgba(239, 237, 232, 0.3);
+  border-radius: 12px;
+  padding: 14px 200px 14px 14px;
+  width: 335px;
+  height: 46px;
+  &::placeholder {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 129%;
+    color: ${(p) => p.theme.colors.white};
+    opacity: 30%;
+  }
+  &:hover,
+  :focus {
+    border-color: ${(p) => p.theme.colors.orange};
+  }
 
   @media screen and (min-width: 1440px) {
     width: 364px;
-height: 52px;
+    height: 52px;
   }
-  `;
-  
-  export const FormGroup = styled.label`
-    display: flex;
-    flex-direction: column;
-    gap: 4px;
-  `;
-  
-  export const ErrorMessage = styled(FormikError)`
-    color: ${p => p.theme.colors.red};
-    font-size: 14px;
-  `;
+`;
 
-  export const Button = styled.button `
+export const FormGroup = styled.label`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+export const ErrorMessage = styled(FormikError)`
+  color: ${(p) => p.theme.colors.red};
+  font-size: 14px;
+`;
+
+export const Button = styled.button`
   width: 130px;
   height: 42px;
   border-radius: 12px;
@@ -79,9 +81,10 @@ height: 52px;
     line-height: 120%;
 
     margin-top: 40px;
-  }`
+  }
+`;
 
-  export const Title = styled.h1`
+export const Title = styled.h1`
   font-weight: 700;
   font-size: 24px;
   line-height: 117%;
@@ -93,10 +96,10 @@ height: 52px;
     font-size: 32px;
     line-height: 138%;
     margin-bottom: 16px;
-  };
-  `
+  }
+`;
 
-  export const Text = styled.h1`
+export const Text = styled.h1`
   font-weight: 400;
   font-size: 14px;
   line-height: 129%;
@@ -109,4 +112,4 @@ height: 52px;
     line-height: 150%;
     margin-bottom: 32px;
   }
-  `
+`;
