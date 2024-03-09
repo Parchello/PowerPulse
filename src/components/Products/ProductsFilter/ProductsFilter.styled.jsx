37cb41b09b7aa14ec03a8809c294541b5913af6a
a.panasenko <andrii.panasenko295@gmail.com>
2024-03-09 13:23:20 +0200

    position bg image

--- src/components/Products/ProductsFilter/ProductsFilter.styled.jsx
+++ src/components/Products/ProductsFilter/ProductsFilter.styled.jsx
@@ -4,7 +4,8 @@ export const MainFiltersContainer = styled.div`
   @media screen and (min-width: 1440px) {
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    /* justify-content: space-between; */
+    gap: 454px;
   }
 `;
 
