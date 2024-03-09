e3481bb6f5277cb3edc555dc49ca9f6a654c1169
a.panasenko <andrii.panasenko295@gmail.com>
2024-03-09 14:14:34 +0200

    styling scrollbar

--- src/components/Products/DietCardsList/DietCardsList.styled.jsx
+++ src/components/Products/DietCardsList/DietCardsList.styled.jsx
@@ -12,6 +12,19 @@ export const DietBlockContainer = styled.ul`
     gap: 32px 16px;
     overflow-y: auto;
     max-height: 500px;
+
+    &::-webkit-scrollbar {
+      width: 8px;
+    }
+    &::-webkit-scrollbar-track {
+      border-radius: 12px;
+      border: 1px solid rgba(239, 237, 232, 0.1);
+    }
+    &::-webkit-scrollbar-thumb {
+      border-radius: 12px;
+      background: ${(p) => p.theme.colors.orangeLight};
+      height: 153px; // ця властивість не застосовується, хз чому.
+    }
   }
 
   @media screen and (min-width: 1440px) {
