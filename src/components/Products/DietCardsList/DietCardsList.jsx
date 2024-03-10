41da25279f6918849446ba0874338b63d2ddcb86
a.panasenko <andrii.panasenko295@gmail.com>
2024-03-10 14:35:22 +0200

    refactor
    виніс модалки продуктів в компонент однієї картки
    Повидаляв закоментований код з попереднього комміту

--- src/components/Products/DietCardsList/DietCardsList.jsx
+++ src/components/Products/DietCardsList/DietCardsList.jsx
@@ -1,61 +1,19 @@
-import avocadoImage from '../../../assets/img/avocado-img.png';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import sprite from '../../../assets/sprite.svg';
-import { ModalProducts } from '../ModalProducts/ModalProducts';
-import {
-  // AddBtn,
-  // DietBlock,
-  DietBlockContainer,
-  // DietInfoHeading,
-  // DietInfoItem,
-  // DietInfoList,
-  // DietInfoValue,
-  // DietName,
-  // DietNameBox,
-  // DietText,
-  // DietTextBox,
-  // RecomendBox,
-  // RecomendStatusCircle,
-  // RecommendStatusText,
-} from './DietCardsList.styled';
-import {
-  AddButton,
-  ButtonBox,
-  CaloriesBox,
-  CaloriesText,
-  CaloriesValue,
-  CancelBox,
-  DietInputBox,
-  DietNameText,
-  GramsInput,
-} from '../ModalProducts/ModalProducts.styled';
-import { ModalWellDone } from '../ModalWellDone/ModalWellDone';
-import {
-  BackBtn,
-  CaloriesWellBox,
-  CaloriesWellText,
-  CaloriesWellValue,
-  ImgBox,
-  NextProductBtn,
-  // RawSvg,
-  WellDoneTitle,
-  WellDonebox,
-} from '../ModalWellDone/ModaWellDone.styled';
+import { DietBlockContainer } from './DietCardsList.styled';
+
 import { getAllProducts } from '../../../redux/products/operations';
 import { ProductsItem } from '../ProductsItem/ProductsItem';
 import {
   selectAllProducts,
   selectIsLoading,
-  // selectVisibleProducts,
+  // selectVisibleProducts, це ще знадобиться
 } from '../../../redux/products/selectors';
 import { Loader } from '../../Loader/Loader';
 
 export const DietCardsList = () => {
   const dispatch = useDispatch();
   const isLoading = useSelector(selectIsLoading);
-  const [modalProductsIsOpen, setModalProductsIsOpen] = useState(false);
-  const [modalWellDoneIsOpen, setModalWellDoneIsOpen] = useState(false);
 
   useEffect(() => {
     dispatch(getAllProducts());
@@ -68,71 +26,20 @@ export const DietCardsList = () => {
 
   return (
     <>
-      {/* Лоадер якогось хєра не застосовується */}
-      {!isLoading && <Loader /> && (
-        <DietBlockContainer>
-          {cards.map((item) => (
+      {/* Лоадер якогось хєра не застосовується 
+      також треба обробити повідомлення про пустий список*/}
+      <DietBlockContainer>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          cards.map((item) => (
             <ProductsItem
               key={item.id}
               value={item}
-              openModal={setModalProductsIsOpen}
             />
-          ))}
-        </DietBlockContainer>
-      )}
-      <ModalProducts
-        isOpen={modalProductsIsOpen}
-        onClose={() => setModalProductsIsOpen(false)}
-      >
-        <DietInputBox>
-          <DietNameText type="text" placeholder="Name of diet" readOnly />
-          <GramsInput type="text" placeholder="grams" />
-        </DietInputBox>
-
-        <CaloriesBox>
-          <CaloriesText>Calories:</CaloriesText>
-          <CaloriesValue>00</CaloriesValue>
-        </CaloriesBox>
-
-        <ButtonBox>
-          <AddButton
-            onClick={() => {
-              setModalProductsIsOpen(false);
-              setModalWellDoneIsOpen(true);
-            }}
-          >
-            Add to diary
-          </AddButton>
-          <CancelBox onClick={() => setModalProductsIsOpen(false)}>
-            Cancel
-          </CancelBox>
-        </ButtonBox>
-      </ModalProducts>
-      <ModalWellDone
-        isOpen={modalWellDoneIsOpen}
-        onClose={() => setModalWellDoneIsOpen(false)}
-      >
-        <ImgBox>
-          <img src={avocadoImage} alt="avocado" />
-        </ImgBox>
-        <div>
-          <WellDonebox>
-            <WellDoneTitle>Well done</WellDoneTitle>
-            <CaloriesWellBox>
-              <CaloriesWellText>Calories:</CaloriesWellText>
-              <CaloriesWellValue>00</CaloriesWellValue>
-            </CaloriesWellBox>
-          </WellDonebox>
-
-          <NextProductBtn>Next product</NextProductBtn>
-          <BackBtn>
-            To the diary
-            <svg width="16px" height="16px">
-              <use xlinkHref={sprite + '#icon-gray-arrow'} />
-            </svg>
-          </BackBtn>
-        </div>
-      </ModalWellDone>
+          ))
+        )}
+      </DietBlockContainer>
     </>
   );
 };
--- src/components/Products/ProductsItem/ProductsItem.jsx
+++ src/components/Products/ProductsItem/ProductsItem.jsx
@@ -1,5 +1,8 @@
 import sprite from '../../../assets/sprite.svg';
-import { RawSvg } from '../ModalWellDone/ModaWellDone.styled';
+import { ModalProducts } from '../ModalProducts/ModalProducts';
+import { AddButton, ButtonBox, CaloriesBox, CaloriesText, CaloriesValue, CancelBox, DietInputBox, DietNameText, GramsInput } from '../ModalProducts/ModalProducts.styled';
+import { BackBtn, CaloriesWellBox, CaloriesWellText, CaloriesWellValue, ImgBox, NextProductBtn, RawSvg, WellDoneTitle, WellDonebox } from '../ModalWellDone/ModaWellDone.styled';
+import { ModalWellDone } from '../ModalWellDone/ModalWellDone';
 import {
   DietBlock,
   DietTextBox,
@@ -15,11 +18,15 @@ import {
   DietInfoHeading,
   DietInfoValue,
 } from './ProductsItem.styled';
+import avocadoImage from '../../../assets/img/avocado-img.png'
+import {useState } from 'react'
 
 export const ProductsItem = ({
   value: { calories, category, title, weight },
-  openModal,
+  // openModal,
 }) => {
+   const [modalProductsIsOpen, setModalProductsIsOpen] = useState(false);
+   const [modalWellDoneIsOpen, setModalWellDoneIsOpen] = useState(false);
   // тут треба ще зробити так, щоб Recommended світився червоним або зеленим
   //в залежності від того яка у користувача група крові
   return (
@@ -31,7 +38,7 @@ export const ProductsItem = ({
         <RecomendBox>
           <RecomendStatusCircle></RecomendStatusCircle>
           <RecommendStatusText>Recommended</RecommendStatusText>
-          <AddBtn onClick={() => openModal(true)}>
+          <AddBtn onClick={() => setModalProductsIsOpen(true)}>
             Add
             <svg width="16px" height="16px">
               <use xlinkHref={sprite + '#icon-red-raw'} />
@@ -59,6 +66,62 @@ export const ProductsItem = ({
           </DietInfoItem>
         </DietInfoList>
       </DietBlock>
+
+      <ModalProducts
+        isOpen={modalProductsIsOpen}
+        onClose={() => setModalProductsIsOpen(false)}
+      >
+        <DietInputBox>
+          <DietNameText type="text" placeholder={title} readOnly />
+          <GramsInput type="text" placeholder="grams" />
+        </DietInputBox>
+
+        <CaloriesBox>
+          <CaloriesText>Calories:</CaloriesText>
+          <CaloriesValue>{calories}</CaloriesValue>
+        </CaloriesBox>
+
+        <ButtonBox>
+          <AddButton
+            onClick={() => {
+              setModalProductsIsOpen(false);
+              setModalWellDoneIsOpen(true);
+            }}
+          >
+            Add to diary
+          </AddButton>
+          <CancelBox onClick={() => setModalProductsIsOpen(false)}>
+            Cancel
+          </CancelBox>
+        </ButtonBox>
+      </ModalProducts>
+      <ModalWellDone
+        isOpen={modalWellDoneIsOpen}
+        onClose={() => setModalWellDoneIsOpen(false)}
+      >
+        <ImgBox>
+          <img src={avocadoImage} alt="avocado" />
+        </ImgBox>
+        <div>
+          <WellDonebox>
+            <WellDoneTitle>Well done</WellDoneTitle>
+            <CaloriesWellBox>
+              <CaloriesWellText>Calories:</CaloriesWellText>
+              <CaloriesWellValue>00</CaloriesWellValue>
+            </CaloriesWellBox>
+          </WellDonebox>
+
+          <NextProductBtn>
+            Next product
+          </NextProductBtn>
+          <BackBtn>
+            To the diary
+            <svg width="16px" height="16px">
+              <use xlinkHref={sprite + '#icon-gray-arrow'} />
+            </svg>
+          </BackBtn>
+        </div>
+      </ModalWellDone>
     </>
   );
 };
