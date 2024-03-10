a462ef48d196621b0909d80c4e336cb12bf52fc8
ksholovko <132094880+ksholovko@users.noreply.github.com>
2024-03-10 14:37:30 +0200

    update

--- src/redux/profile/profileSlice.jsx
+++ src/redux/profile/profileSlice.jsx
@@ -28,6 +28,7 @@ export const ProfileSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(fetchCurrentUser.fulfilled, (state, action) => {
+        console.log('action payload in reducer', action.payload);
         state.currentUser = action.payload;
         state.isLoading = false;
         state.error = null;
